f4f7c02caf23aa01b113b7ccbec5dc54fa1c933d
Gabrielle <gabrielle.pierre19@hotmail.fr>
2022-05-25 01:01:38 +0200

    brouilloon

--- src/final/Final.js
+++ src/final/Final.js
@@ -13,6 +13,54 @@ export const Final = () => {
           </h1>
         </div>
       </div>
+      <section className="max-w-8xl mx-auto container bg-white pt-16">
+        <div role="contentinfo" className="flex items-center flex-col px-4">
+          <h2 className="focus:outline-none border-b-2 text-2xl pb-10 mb-20 lg:pb-16 lg:mb-28 lg:text-4xl font-extrabold text-center leading-8 text-amber-600 lg:w-5/12 md:w-9/12 pt-4">
+            Présentation détaillée du sujet de stage
+          </h2>
+        </div>
+        <div className="lg:flex lg:flex-row">
+          <div className="pr-5">
+            <h3 className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">
+              Prise de contact
+            </h3>
+            <p className="focus:outline-none text-base text-gray-600 leading-normal pt-5 pb-10">
+              Le 9 mars 2022, j’ai visité le site de la French Tech de Clermont
+              Ferrand et c’est ici que j’ai découvert l’entreprise. J’ai donc
+              envoyé ma candidature pour le stage et nous avons convenu un
+              entretien pour le lundi 14 mars. À la suite de cet entretien,
+              l’entreprise a répondu favorablement à ma candidature. Mon stage a
+              ainsi pu débuter le 28 mars.{" "}
+            </p>
+          </div>
+          <div className="lg:px-5 lg:border-l-2">
+            <h3 className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">
+              L'équipe
+            </h3>
+            <p className="focus:outline-none text-base text-gray-600 leading-normal pt-5 pb-10">
+              L’entreprise est constituée de 12 personnes. L’équipe de
+              développement est composée du responsable de service (Jérémy
+              Paris, mon tuteur de stage), d’un ingénieur IT (Ozcan Sevik), et
+              de trois développeurs full stack (Benjamin Caure, Roméo Brilland
+              et Mao De Matos). Je serai intégrée à cette équipe.
+            </p>
+          </div>
+          <div className="lg:pl-5 lg:border-l-2">
+            <h3 className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">
+              Sujet du stage
+            </h3>
+            <p className="focus:outline-none text-base text-gray-600 leading-normal pt-5 pb-10">
+              Je serais associée à différents projets (internes ou à la
+              prestation de service), pour la participation à la conception,
+              l’architecture, l’analyse et le développement d’applications web
+              et mobile. J'utiliserais de technologies de développement modernes
+              tels que la stack MERN (Mongo Express React Node) ou PHP. Mon
+              projet principal sera l'amélioration d'une application métier en
+              React.js pour le groupe LGMN.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
