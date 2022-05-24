447281b319efaf588a29cc6f1394bac926fdcdf6
Gabrielle <gabrielle.pierre19@hotmail.fr>
2022-05-24 22:18:53 +0200

    Update Final.js

--- src/final/Final.js
+++ src/final/Final.js
@@ -13,19 +13,6 @@ export const Final = () => {
           </h1>
         </div>
       </div>
-
-      <div role="contentinfo" className="flex items-center flex-col p-4">
-        <h2 className="focus:outline-none border-b-2 text-2xl pb-10 mb-20 lg:pb-16 lg:mb-28 lg:text-4xl font-extrabold text-center leading-8 text-amber-600 lg:w-5/12 md:w-9/12 pt-4">
-          Il n'y a rien ici pour le moment.
-        </h2>
-      </div>
-      <div className="relative flex flex-col justify-center items-center">
-        <Link to="/">
-          <button className="w-full sm:w-auto text-base leading-4 text-center text-white py-6 px-16 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-gray-800 hover:bg-gray-500 ">
-            Retourner à l'accueil
-          </button>
-        </Link>
-      </div>
     </div>
   );
 };
