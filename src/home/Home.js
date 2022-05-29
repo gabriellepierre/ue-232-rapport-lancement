f4431725811198b072a086ae0d3dbc81c5deea23
Gabrielle <gabrielle.pierre19@hotmail.fr>
2022-05-29 21:14:39 +0200

    Update Home.js

--- src/home/Home.js
+++ src/home/Home.js
@@ -52,7 +52,7 @@ const Home = () => {
         <Cards />
         <div className="border-b-2 pt-10 m-20 flex"></div>
         <div className="w-full lg:mx-40 p-5 flex flex-col justify-center">
-          <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 flex justify-center">
+          <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 flex">
             Remerciements
           </h1>
           <p className="text-lg leading-6 text-gray-600 ">
