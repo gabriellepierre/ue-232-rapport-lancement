900fbbbab398af74f367e0a941335d0486d52e45
Gabrielle <gabrielle.pierre19@hotmail.fr>
2022-05-29 12:20:10 +0200

    Update Final.js

--- src/final/Final.js
+++ src/final/Final.js
@@ -1,7 +1,7 @@
 // import React from "react";
 
-import { Link } from "../shared/Link";
-import { Planning } from "./Planning";
+// import { Link } from "../shared/Link";
+// import { Planning } from "./Planning";
 
 export const Final = () => {
   return (
